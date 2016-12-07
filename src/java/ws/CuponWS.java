c30d025355e2baf961070aeb1a5c32a5f8848305
Bruo Estrada <brunoeb98@gmail.com>
2016-12-06 23:43:57 -0600

    Merge origin/master
    Conflicts:
    	src/main/java/com/hibernate/entidades/Telefono.java
    	src/main/java/com/hibernate/tallerm/App.java

---
+++ src/java/ws/CuponWS.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import mybatis.MyBatisUtil;
+import org.apache.ibatis.session.SqlSession;
+import pojos.Cupon;
+import pojos.Mensaje;
+import pojos.Oferta;
+import pojos.Usuario;
+
+/**
+ * REST Web Service
+ *
+ * @author bruno
+ */
+@Path("cupones")
+public class CuponWS {
+    @Path("byidusuario/{idusuario}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Cupon> cuponesByIdusuario(
+    @PathParam("idusuario") Integer idsuario){
+        List<Cupon> list =null;
+        SqlSession conn =MyBatisUtil.getSession();
+        if(conn!=null){
+            try{
+                list=conn.selectList("Cupon.cuponesByIdusuario", idsuario);
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }finally{
+                conn.close();
+            }
+        }           
+        return list;
+    }
+    
+    @POST
+    @Path("generacupon")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Mensaje generaCupon(
+            @FormParam("idusuario") Integer idusuario,
+            @FormParam("idoferta") Integer idoferta
+         ){
+        Mensaje resultado;
+        DateFormat fecha=new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date fechacreacion= new Date();
+        Cupon c=new Cupon(idusuario, fechacreacion, idoferta);
+        SqlSession conn = MyBatisUtil.getSession();
+        try{
+            conn.insert("Cupon.generaCupon",c);
+            conn.commit();
+            resultado=new Mensaje("Cupon creado exitosamente", false);
+        }catch(Exception ex){
+            resultado=new Mensaje(ex.getMessage(),true);
+        }finally{
+            conn.close();
+        }
+        return resultado;
+        
+    }
+
+
+    
+
+}
